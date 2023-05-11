6c315c17b4eda2ef069a3475a9ece359e3f6b060
unknown <serpaez36123@gmail.com>
2023-05-11 20:53:08 -0300

    jueves 11

--- src/Semana.java
+++ src/Semana.java
@@ -9,30 +9,30 @@ public class Semana {
         System.out.println("INGRESE NUM");
         numero=in.nextInt();
         switch (numero) {
-            case 1:
-                System.out.println("dia lunes");
+            case 1: System.out.println("dia lunes");
                 break;
 
-            case 2:
-                System.out.println("dia martes");
+
+            case 2:  System.out.println("dia martes");
                 break;
 
-            case 3:
-                System.out.println("dia miercoles");
-                break;
-            case 4:
-                System.out.println("dia jueves");
+
+            case 3: System.out.println("dia miercoles");
                 break;
 
-            case 5:
-                System.out.println("dia viernes");
+            case 4: System.out.println("dia jueves");
                 break;
-            case 6:
-                System.out.println("dia sabado");
+
+
+            case 5: System.out.println("dia viernes");
                 break;
-            case 7:
-                System.out.println("dia domingo");
+
+            case 6:  System.out.println("dia sabado");
                 break;
+
+            case 7:   System.out.println("dia domingo");
+                break;
+
             default:System.out.println("no es un dia de la semana");
 
 
